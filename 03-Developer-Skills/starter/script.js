0782bc5124ede0106eabfdc3e109b516ba13121e
Aaron <102453885+Dsyleixc@users.noreply.github.com>
2024-06-18 23:05:01 -0400

    Finished 03 Developer Skills, also did a bunch of problems given by friends to practice fundamentals

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -65,10 +65,10 @@ const maxMinObject = {
 let tempMax = maxMinObject.max(temperatures);
 let tempMin = maxMinObject.min(temperatures);
 console.log(tempMax);
-*/
+
 
 // Find common prefix
-const strs = ['floser', 'flos', 'flisht'];
+const strs = ['flower', 'flow', 'flight'];
 
 const shortest = function (array) {
     let varLength = array[0].length;
@@ -119,3 +119,121 @@ commonPrefix(strs);
 
 //   const kelvin = measurement.value * 273
 // };
+
+
+// Problem 1
+// const finalArray = [];
+
+// const row = 6;
+// const column = 4;
+
+// for loop for each spot in array
+// for (let i = 0; i < column; i++) {
+//     finalArray[i] = [];
+//     // Loop 7 times to print
+//     for (let j = 0; j < row; j++) {
+//         // if array spot is even X if odd O
+//         if (i % 2 === 0) {
+
+//             finalArray[i].push('X');
+//         } else {
+//             finalArray[i].push('0');
+//         }
+//     }
+// }
+// console.log(finalArray);
+
+// Problem 2
+// const finalArray = [];
+
+// const row = 5;
+// const column = 4;
+// const firstLetter = 'X';
+// // Loop to create the 4 arrays
+// for (let i = 0; i < column; i++) {
+//     finalArray[i] = [];
+//     finalArray[i].push(firstLetter);
+//     // Loop 7 times to print within the array
+//     for (let j = 0; j < row; j++) {
+//         // if array spot j - 1 === 'X' then O
+//         if (finalArray[i][j] === 'X') {
+//             finalArray[i].push('O');
+//         } else {
+//             finalArray[i].push('X');
+//         }
+//     }
+// }
+// console.log(finalArray);
+
+
+//Problem 3
+const finalArray = [];
+
+const row = 4;
+const column = 6;
+const firstLetter = 'X';
+// Loop to create the 4 arrays
+for (let i = 0; i < row; i++) {
+    finalArray[i] = [];
+    // if in first array position start it with firstLetter
+    if (i === 0) {
+        finalArray[i].push(firstLetter);
+        // if first letter of previous array = firstLetter then start the new array with 'O'
+    } else if (finalArray[i - 1][0] === firstLetter) {
+        finalArray[i].push('O');
+        // if none of above work then that means you need to push a 'X'
+    } else {
+        finalArray[i].push(firstLetter);
+    }
+
+    // Loop 7 times to print within the array
+    for (let j = 0; j < column - 1; j++) {
+        // if array spot j - 1 === 'X' then O
+        if (finalArray[i][j] === 'X') {
+            finalArray[i].push('O');
+        } else {
+            finalArray[i].push('X');
+        }
+    }
+}
+console.log(finalArray);
+
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+const nums = [2, 7, 11, 15];
+// // loop for first number
+// for (let i = 0; i < nums.length; i++) {
+//     // loop for second number
+//     for (let j = 0; j < nums.length; j++) {
+//         if (nums[i] + nums[j] === 9) {
+//             console.log([i, j]);
+//         }
+//     }
+// }
+
+let obj = {};
+let target = 9;
+for (let i = 0; i < nums.length; i++) {
+    if (obj[target - nums[i]] !== undefined) {
+        console.log([obj[target - nums[i]], i]);
+    }
+    obj[nums[i]] = i;
+    console.log(obj);
+}
+*/
+
+const arr = [12, 5, -5, 0, 4];
+
+let tempScript = '';
+const printForcast = function (tempArray) {
+    for (let i = 0; i < arr.length; i++) {
+        let temp = `... ${arr[i]}C in ${arr.indexOf(arr[i]) + 1} days `;
+        tempScript += temp;
+    }
+    console.log(tempScript);
+};
+
+console.log(printForcast(arr));
