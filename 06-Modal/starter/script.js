57db3e866944d817d6e51d0fa6b81cade897e996
Aaron <102453885+Dsyleixc@users.noreply.github.com>
2024-06-20 18:33:48 -0400

    finished 06-Modal, did html and css on my own learned that for modal its fine to use absolute position to place it where you want. Also made modal open and close via .JS

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeButton = document.querySelector('.close-modal');
+const openModalButton = document.querySelectorAll('.show-modal');
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < openModalButton.length; i++) {
+    // OPEN MODAL AND OVERLAY WHEN CLICKED
+    openModalButton[i].addEventListener('click', openModal);
+}
+
+// CLOSE MODAL AND OVERLAY WHEN CLOSE BUTTON OR OVERLAY CLICKED
+closeButton.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// CLOSE MODAL AND OVERLAY WHEN ESC CLICKED
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
